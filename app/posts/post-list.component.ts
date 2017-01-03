954a9f6fa6de77372838a34402a1dd22c33f2eeb
sguzunov <sguzunov@gmail.com>
2017-01-03 08:00:22 +0200

    List of posts shows images.

--- app/posts/post-list.component.ts
+++ app/posts/post-list.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 import { IPost } from './post';
 import { PostService } from './post.service';
+import { Constants } from './../constants/constants';
 
 @Component({
     templateUrl: 'app/posts/post-list.component.html',
 })
 
 export class PostListComponent implements OnInit {
+    public apiEndPoint: String = Constants.imagesUrl;
     posts: IPost[]
 
     constructor(private _postService: PostService) {
