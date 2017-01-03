18a81e6d02ef9c16b5a21c25fa7b42152f36bb97
sguzunov <sguzunov@gmail.com>
2017-01-03 02:04:26 +0200

    Create post successfully.

--- app/posts/post/post.component.js
+++ app/posts/post/post.component.js
@@ -10,18 +10,66 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var user_service_1 = require("./../../services/user-service");
+var post_service_1 = require("./../post.service");
 var DEFAULT_CATEGORY = 'other';
+var Post = (function () {
+    function Post(title, category, author, createdOn, imageName, imagePath, likes, dislikes, comments) {
+        if (comments === void 0) { comments = null; }
+        this.title = title;
+        this.category = category;
+        this.author = author;
+        this.createdOn = createdOn;
+        this.imageName = imageName;
+        this.imagePath = imagePath;
+        this.likes = likes;
+        this.dislikes = dislikes;
+        this.comments = comments;
+    }
+    return Post;
+}());
 var PostCreateComponent = (function () {
-    function PostCreateComponent(userService) {
+    function PostCreateComponent(userService, postService) {
         this.userService = userService;
+        this.postService = postService;
         this.selectedCategory = DEFAULT_CATEGORY;
         this.categories = ['fun', 'chilling', 'crazy', 'extreme', 'party',
             'celebration', 'kifla', 'other'];
     }
     PostCreateComponent.prototype.createPost = function () {
-        if (!this.postTitle) {
-            console.log('No title');
+        var _this = this;
+        if (this.imageName == '' || this.imagePath == '') {
+            this.handleError('No image uploaded. Please upload image before posting!');
+            return;
         }
+        if (!this.title || this.title === '') {
+            this.handleError('Title is missing!');
+            return;
+        }
+        this.userService.getLoggedUser()
+            .subscribe(function (res) {
+            if (!res.user) {
+                _this.handleError('No user is logged in!');
+                return;
+            }
+            var author = res.user.username;
+            var post = new Post(_this.title, _this.selectedCategory, author, new Date(), _this.imageName, _this.imagePath, 0, 0);
+            _this.postService.createPost(post)
+                .subscribe(function (dbPost) { return _this.userService.addPost(author, dbPost); });
+        });
+    };
+    PostCreateComponent.prototype.onUploaded = function (file) {
+        if (file !== undefined) {
+            this.imageName = file.filename;
+            this.imagePath = file.path;
+        }
+    };
+    PostCreateComponent.prototype.handleError = function (msg) {
+        var _this = this;
+        this.error = true;
+        this.errorMessage = msg;
+        return setTimeout(function () {
+            _this.error = false;
+        }, 2500);
     };
     return PostCreateComponent;
 }());
@@ -30,7 +78,7 @@ PostCreateComponent = __decorate([
         moduleId: module.id,
         templateUrl: './post.component.html'
     }),
-    __metadata("design:paramtypes", [user_service_1.UserService])
+    __metadata("design:paramtypes", [user_service_1.UserService, post_service_1.PostService])
 ], PostCreateComponent);
 exports.PostCreateComponent = PostCreateComponent;
 //# sourceMappingURL=post.component.js.map
--- app/posts/post/post.component.ts
+++ app/posts/post/post.component.ts
@@ -1,26 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { UserService } from './../../services/user-service';
+import { PostService } from './../post.service';
+import { PostUploadComponent } from './../shared/post-upload.component';
+import { IPost } from './../post';
 
 const DEFAULT_CATEGORY: String = 'other';
+
+class Post implements IPost {
+    constructor(public title: String,
+        public category: String,
+        public author: String,
+        public createdOn: Date,
+        public imageName: String,
+        public imagePath: String,
+        public likes: Number,
+        public dislikes: Number,
+        public comments: String[] = null
+    ) { }
+}
 
 @Component({
     moduleId: module.id,
     templateUrl: './post.component.html'
 })
 export class PostCreateComponent {
-    public postTitle: String;
+    private author: String;
+    private imageName: String;
+    private imagePath: String;
+    private post: Post;
+
+    public title: String;
     public selectedCategory: String = DEFAULT_CATEGORY;
     public categories: String[];
+    public error: Boolean;
+    public errorMessage: String;
 
-    constructor(private userService: UserService) {
+    constructor(private userService: UserService, private postService: PostService) {
         this.categories = ['fun', 'chilling', 'crazy', 'extreme', 'party',
             'celebration', 'kifla', 'other'];
     }
 
-    public createPost() {
-        if (!this.postTitle) {
-            console.log('No title');
+    public createPost(): void {
+        if (this.imageName == '' || this.imagePath == '') {
+            this.handleError('No image uploaded. Please upload image before posting!');
+            return;
         }
+
+        if (!this.title || this.title === '') {
+            this.handleError('Title is missing!');
+            return;
+        }
+
+        this.userService.getLoggedUser()
+            .subscribe((res) => {
+                if (!res.user) {
+                    this.handleError('No user is logged in!');
+                    return;
+                }
+
+                let author = res.user.username;
+                let post = new Post(this.title, this.selectedCategory, author, new Date(), this.imageName, this.imagePath, 0, 0);
+                this.postService.createPost(post)
+                    .subscribe((dbPost) => this.userService.addPost(author, dbPost));
+            });
+    }
+
+    public onUploaded(file: any): void {
+        if (file !== undefined) {
+            this.imageName = file.filename;
+            this.imagePath = file.path;
+        }
+    }
+
+    private handleError(msg: String) {
+        this.error = true;
+        this.errorMessage = msg;
+        return setTimeout(() => {
+            this.error = false;
+        }, 2500);
     }
 }
